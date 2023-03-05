9abda63361dd942792c3fa5c14309cada41fa220
junfeng <sun_0901@foxmail.com>
2023-03-05 17:36:46 +0800

    init project with docker configs

---
+++ src/main/java/com/example/pethospitalbackend/PetHospitalBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.pethospitalbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetHospitalBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PetHospitalBackendApplication.class, args);
+    }
+
+}
