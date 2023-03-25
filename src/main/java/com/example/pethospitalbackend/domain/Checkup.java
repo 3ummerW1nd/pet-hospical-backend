62fd959c13ba6cf4475c6941a90548227dd6299e
junfeng <sun_0901@foxmail.com>
2023-03-26 03:21:44 +0800

    add azure cognitive search index

--- src/main/java/com/example/pethospitalbackend/domain/Checkup.java
+++ src/main/java/com/example/pethospitalbackend/domain/Checkup.java
@@ -1,5 +1,9 @@
 package com.example.pethospitalbackend.domain;
 
+import com.azure.search.documents.indexes.SearchableField;
+import com.azure.search.documents.indexes.SimpleField;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -14,17 +18,24 @@ import javax.persistence.*;
 @AllArgsConstructor
 @NoArgsConstructor
 @DynamicUpdate
+@JsonInclude(JsonInclude.Include.NON_NULL)
 @Table(name = "checkups")
 public class Checkup {
     @Id
     @Column(name = "id", nullable = false, updatable = false)
     @GeneratedValue(strategy = GenerationType.AUTO)
+    @JsonProperty("id")
+    @SimpleField(isKey = true)
     private Integer id;
 
     @Column(name = "name", nullable = false, columnDefinition = "VARCHAR(64)")
+    @JsonProperty("name")
+    @SearchableField(analyzerName = "zh-Hans.microsoft")
     private String name;
 
     @Column(name = "introduction", nullable = false, columnDefinition = "VARCHAR(2048)")
+    @SearchableField(analyzerName = "zh-Hans.microsoft")
+    @JsonProperty("introduction")
     private String introduction;
 
     @Column(name = "price", nullable = false, columnDefinition = "Decimal(8, 2)")
--- src/main/java/com/example/pethospitalbackend/domain/Department.java
+++ src/main/java/com/example/pethospitalbackend/domain/Department.java
@@ -1,5 +1,9 @@
 package com.example.pethospitalbackend.domain;
 
+import com.azure.search.documents.indexes.SearchableField;
+import com.azure.search.documents.indexes.SimpleField;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -14,23 +18,32 @@ import javax.persistence.*;
 @AllArgsConstructor
 @NoArgsConstructor
 @DynamicUpdate
+@JsonInclude(JsonInclude.Include.NON_NULL)
 @Table(name = "departments",
         indexes = {@Index(name = "my_index_name",  columnList="name", unique = true)})
 public class Department {
     @Id
     @Column(name = "id", nullable = false, updatable = false)
     @GeneratedValue(strategy = GenerationType.AUTO)
+    @JsonProperty("id")
+    @SimpleField(isKey = true)
     private Integer id;
 
     @Column(name = "name", nullable = false, columnDefinition = "VARCHAR(64)")
+    @SearchableField(analyzerName = "zh-Hans.microsoft")
+    @JsonProperty("name")
     private String name;
 
     @Column(name = "phoneNumber", nullable = false, columnDefinition = "VARCHAR(64)")
+    @SearchableField()
+    @JsonProperty("phoneNumber")
     private String phoneNumber;
 
     @Column(name = "directorId", nullable = false)
     private Integer directorId;
 
     @Column(name = "functions", nullable = false, columnDefinition = "VARCHAR(2048)")
+    @SearchableField(analyzerName = "zh-Hans.microsoft")
+    @JsonProperty("functions")
     private String functions;
 }
