0a63b3bc7135265d5672aabb2084e9e354a5d620
牛雅可 <1203360373@qq.com>
2023-04-16 23:08:35 +0800

    4.16

--- src/main/java/com/example/pethospitalbackend/PetHospitalBackendApplication.java
+++ src/main/java/com/example/pethospitalbackend/PetHospitalBackendApplication.java
@@ -12,26 +12,26 @@ import org.springframework.scheduling.annotation.EnableAsync;
 
 import java.io.File;
 
-@Configuration
-@ServletComponentScan
+//@Configuration
+//@ServletComponentScan
 @EnableAsync
 @SpringBootApplication
 @EnableJpaRepositories(basePackages = "com.example.pethospitalbackend.repository")
 @EntityScan("com.example.pethospitalbackend.domain")
 public class PetHospitalBackendApplication {
-    @Value("${spring.servlet.multipart.location}")
-    private  String tempDir;
-
-    @Bean("mkdir")
-    public void mkDir(){
-        File file = new File(tempDir);
-        if (file.exists()){
-            System.out.println("文件夹存在");
-        }else {
-            System.out.println("文件夹不存在");
-            file.mkdirs(); //创建文件夹
-        }
-    }
+//    @Value("${spring.servlet.multipart.location}")
+//    private  String tempDir;
+//
+//    @Bean("mkdir")
+//    public void mkDir(){
+//        File file = new File(tempDir);
+//        if (file.exists()){
+//            System.out.println("文件夹存在");
+//        }else {
+//            System.out.println("文件夹不存在");
+//            file.mkdirs(); //创建文件夹
+//        }
+//    }
 
     public static void main(String[] args) {
         SpringApplication.run(PetHospitalBackendApplication.class, args);
