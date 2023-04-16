7543e2742a7df85133a827c235048430873bfae2
牛雅可 <1203360373@qq.com>
2023-04-16 22:39:51 +0800

    4.16

--- src/main/java/com/example/pethospitalbackend/PetHospitalBackendApplication.java
+++ src/main/java/com/example/pethospitalbackend/PetHospitalBackendApplication.java
@@ -1,16 +1,37 @@
 package com.example.pethospitalbackend;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableAsync;
 
+import java.io.File;
+
+@Configuration
+@ServletComponentScan
 @EnableAsync
 @SpringBootApplication
 @EnableJpaRepositories(basePackages = "com.example.pethospitalbackend.repository")
 @EntityScan("com.example.pethospitalbackend.domain")
 public class PetHospitalBackendApplication {
+    @Value("${spring.servlet.multipart.location}")
+    private  String tempDir;
+
+    @Bean("mkdir")
+    public void mkDir(){
+        File file = new File(tempDir);
+        if (file.exists()){
+            System.out.println("文件夹存在");
+        }else {
+            System.out.println("文件夹不存在");
+            file.mkdirs(); //创建文件夹
+        }
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(PetHospitalBackendApplication.class, args);
